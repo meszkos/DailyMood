f07ea991f7d9cbdf8e83f8f92e56e68eef31e573
meszkos <michalik515@gmail.com>
2022-06-13 18:44:28 +0200

    Initial Commit

---
+++ DailyMood/DailyMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodApp.swift
+//  DailyMood
+//
+//  Created by MacOS on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DailyMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
